f0f6238d14ca1699a692c641459eac8986781d94
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-08 04:48:00 +0100

    Refactor payment method model to use 255 characters for id column and improve code readability

--- models/payment_method_model.py
+++ models/payment_method_model.py
@@ -1,13 +1,15 @@
-from ..db.session import Base
-from ..db.base import ModelBase
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, JSON, Enum
+from db.session import Base
+from db.base import ModelBase
+from enum import Enum
+from sqlalchemy import Column, String, Boolean, JSON, Enum as SQLEnum
 from datetime import datetime
 from uuid import uuid4
 
 
 def generate_id():
     """Generates a random id"""
-    id = f"pm_{str(uuid4())[:8]}"
+    id = f"pm_{str(uuid4())[:14]}"
+    return id
 
 
 class PaymentMethodType(Enum):
@@ -18,10 +20,10 @@ class PaymentMethodType(Enum):
 
 class PaymentMethod(ModelBase, Base):
     __tablename__ = "payment_methods"
-    id = Column(String, primary_key=True, default=generate_id, index=True)
+    id = Column(String(255), primary_key=True, default=generate_id, index=True)
     is_active = Column(Boolean, default=True)
     method_type = Column(
-        Enum(PaymentMethodType, name="payment_method_types"),
+        SQLEnum(PaymentMethodType, name="payment_method_types"),
         nullable=False,
     )
     details = Column(
