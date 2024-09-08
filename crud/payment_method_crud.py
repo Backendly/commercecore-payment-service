36f19be713c3bfc513e4fc9d2552716064a67ba4
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-08 03:02:30 +0100

    Refactor payment method CRUD functions to use async session and improve code readability

--- crud/payment_method_crud.py
+++ crud/payment_method_crud.py
@@ -32,3 +32,29 @@ async def get_payment_method(id: str):
             select(PaymentMethod).filter(PaymentMethod.id == id)
         )
         return result.scalar_one()
+
+
+async def update_payment_method(id: str, payment_method: PaymentMethodUpdate):
+    """Updates a payment method"""
+    async with get_db() as session:
+        result = await session.execute(
+            select(PaymentMethod).filter(PaymentMethod.id == id)
+        )
+        payment_method_db = result.scalar_one()
+        for key, value in payment_method.model_dump().items():
+            setattr(payment_method_db, key, value)
+        await session.commit()
+        await session.refresh(payment_method_db)
+        return payment_method_db
+
+
+async def delete_payment_method(id: str):
+    """Deletes a payment method"""
+    async with get_db() as session:
+        result = await session.execute(
+            select(PaymentMethod).filter(PaymentMethod.id == id)
+        )
+        payment_method = result.scalar_one()
+        session.delete(payment_method)
+        await session.commit()
+        return payment_method
