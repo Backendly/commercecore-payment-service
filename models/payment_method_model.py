3360c9313d7589ce1da5e5902284b237c27cf9d8
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-08 02:47:06 +0100

    Refactor PaymentMethod model to include method_type as an Enum and details as a JSON column

--- models/payment_method_model.py
+++ models/payment_method_model.py
@@ -2,14 +2,26 @@ from ..db.session import Base
 from ..db.base import ModelBase
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, JSON, Enum
 from datetime import datetime
+from uuid import uuid4
+
+
+def generate_id():
+    """Generates a random id"""
+    id = f"pm_{str(uuid4())[:8]}"
+
+
+class PaymentMethodType(Enum):
+    card = "card"
+    paypal = "paypal"
+    stripe = "stripe"
 
 
 class PaymentMethod(ModelBase, Base):
     __tablename__ = "payment_methods"
-
+    id = Column(String, primary_key=True, default=generate_id, index=True)
     is_active = Column(Boolean, default=True)
     method_type = Column(
-        Enum("card", "paypal", "stripe", name="payment_method_types"),
+        Enum(PaymentMethodType, name="payment_method_types"),
         nullable=False,
     )
     details = Column(
