abe234e028810a17fe90d7149baabecdad2719cd
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-08 02:46:42 +0100

    Refactor ModelBase class in db/base.py to update the default and onupdate values for created_at and updated_at columns

--- db/base.py
+++ db/base.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from datetime import datetime, timezone
 
 Base = declarative_base()
 
@@ -7,6 +8,10 @@ Base = declarative_base()
 class ModelBase:
     """Base class for all models"""
 
-    id = Column(Integer, primary_key=True, index=True)
-    created_at = Column(DateTime, nullable=False)
-    updated_at = Column(DateTime, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.now)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        default=None,
+        onupdate=datetime.now,
+    )
