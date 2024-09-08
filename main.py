327f016851897e314ee890cab80107526dc19314
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-08 19:12:51 +0100

    Refactor main.py to include payment method routes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from db.session import create_all_tables
+from routes.payment_method_routes import router as payment_method_router
 
 
 app = FastAPI(lifespan=create_all_tables)
+app.include_router(payment_method_router)
