814d43a1f3c7625b61e7fd4937e35d45f9db8d6c
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-10 18:56:23 +0100

    Refactor import statement in db/base.py to use sqlalchemy.orm instead of sqlalchemy.ext.declarative
    Instead of importing from the sqlalchemy.ext.declarative, I imported from the sqlalchemy.orm
    which is the latest version

--- db/base.py
+++ db/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from datetime import datetime, timezone
 
