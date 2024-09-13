e56a1b05ae08f96e2b8831bd55ee2a79d9adcd23
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-13 16:55:11 +0100

    Refactor transaction_crud.py to remove unused functions and improve error handling

--- crud/transaction_crud.py
+++ crud/transaction_crud.py
@@ -8,73 +8,6 @@ from schema.transaction_schema import InitiatePaymentTransaction
 from datetime import datetime
 
 
-async def continue_onboarding(data: Request, session: AsyncSession):
-    """Continues the onboarding process"""
-    data = await data.json()
-    account_id = data["account_id"]
-
-    try:
-        onboarding = stripe.AccountLink.create(
-            account=account_id,
-            refresh_url="http://localhost:8000",
-            return_url="http://localhost:8000",
-            type="account_onboarding",
-        )
-    except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail=f"An error occurred while creating the onboarding link.{e}",
-        )
-
-    timestamp = datetime.fromtimestamp(onboarding["expires_at"])
-    date = f"{timestamp.hour}"
-    return {
-        "onboarding_url": onboarding.url,
-        "message": f"Onboarding link expires in {date} hours use the"
-        f" 'onboarding_url' to complete the onboarding",
-    }
-
-
-async def create_connected_account(data: Request, session: AsyncSession):
-    """Creates a connected account"""
-    data = await data.json()
-    developer_id = data["developer_id"]
-    email = data["email"]
-
-    try:
-        account = stripe.Account.create(
-            country="US",
-            email=email,
-            controller={
-                "fees": {"payer": "application"},
-                "losses": {"payments": "application"},
-                "stripe_dashboard": {"type": "express"},
-            },
-            metadata={"developer_id": developer_id},
-        )
-    except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail=f"An error occurred while creating the connected account.{e}",
-        )
-
-    full_onboard_url = stripe.AccountLink.create(
-        account=account["id"],
-        refresh_url="http://localhost:8000",
-        return_url="http://localhost:8000",
-        type="account_onboarding",
-    )
-
-    timestamp = datetime.fromtimestamp(full_onboard_url["expires_at"])
-    date = f"{timestamp.hour}"
-    return {
-        "onboarding_url": full_onboard_url.url,
-        "account_id": account["id"],
-        "message": f"Connected account created successfully onboarding link expires in {date} hours"
-        f" use the 'onboarding_url' to complete the onboarding",
-    }
-
-
 async def initiate_payment_transaction(payment: Request, session: AsyncSession):
     """Initiates a payment transaction"""
     data = await payment.json()
@@ -142,7 +75,9 @@ async def payment_confirmation(data: Request, session: AsyncSession):
     except Exception as e:
         raise HTTPException(
             status_code=500,
-            detail=f"An error occurred while confirming the payment intent.{e}",
+            detail=f"An error occurred while confirming the payment intent{e}"
+            f" Check if the payment method is valid, and also check if you have"
+            f"completed the onboarding process",
         )
 
     transaction = (
