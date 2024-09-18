f1b62a2f9e4a3e6db1c27315166027a4cb0cc450
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-18 12:52:57 +0100

    Refactor backgrounds.py to include async/await for storing user and developer data in Redis and add save_transaction function

--- backgrounds.py
+++ backgrounds.py
@@ -1,11 +1,17 @@
 from redis_db.redis_db import redis_instance
+from models.transaction_model import Transaction
 
 
-def store_user_data(key: str, value: str):
+async def store_user_data(key: str, value: str):
     """Store user data in Redis"""
-    redis_instance.set(key, value)
+    await redis_instance.set(key, value)
 
 
-def store_developer_data(key: str, value: str):
+async def store_developer_data(key: str, value: str):
     """Store developer data in Redis"""
-    redis_instance.set(key, value)
+    await redis_instance.set(key, value)
+
+
+def save_transaction(transaction: Transaction):
+    """Save transaction in Redis"""
+    redis_instance.set(transaction.transaction_id, transaction)
