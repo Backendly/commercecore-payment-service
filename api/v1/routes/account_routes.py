160fac00500f57cf90360f0f7912d724f8aa068c
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-20 23:16:34 +0100

    Refactor initiate_payment_transaction function to use background tasks

--- api/v1/routes/account_routes.py
+++ api/v1/routes/account_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, HTTPException
 from db.session import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.background import BackgroundTasks
@@ -27,55 +27,80 @@ async def connected_account(
     """create connected accounts"""
     developer_id = None
     client = redis_instance()
-    if data.headers.get("X-Developer-ID"):
-        if developer_id := client.get(data.headers.get("X-Developer-ID")):
-            developer_id = developer_id.decode("utf-8")
+    if data.headers.get("X-Developer-Token"):
+        if developer_id := client.get(data.headers.get("X-Developer-Token")):
+            data_response = await create_connected_account(
+                data=data, session=session, validated_developer=developer_id
+            )
+            onboarding_url = data_response["onboarding_url"]
+            del data_response["onboarding_url"]
+            links = {
+                "self": str(data.url),
+                "onboarding_url": onboarding_url,
+            }
+
+            meta = {"status_code": 201}
+
+            return ConnectedAccountResponse(links=links, meta=meta, data=data_response)
         else:
-            developer_id = await validate_developer(
+            validated_developer = await validate_developer(
                 data, background_tasks=background_tasks
             )
-    if developer_id := data.headers.get("X-Developer-ID"):
-        developer_id = redis_instance.get(developer_id)
+            data_response = await create_connected_account(
+                data, session, validated_developer
+            )
+            onboarding_url = data_response["onboarding_url"]
+            del data_response["onboarding_url"]
+            links = {
+                "self": str(data.url),
+                "onboarding_url": onboarding_url,
+            }
+
+            meta = {"status_code": 201}
+
+            return ConnectedAccountResponse(links=links, meta=meta, data=data_response)
     else:
-        developer_id = await validate_developer(data, background_tasks=background_tasks)
-
-    # Add the developer_id to the data
-    data_response = await create_connected_account(
-        data=data, session=session, developer_id=developer_id
-    )
-    onboarding_url = data_response["onboarding_url"]
-    del data_response["onboarding_url"]
-    links = {
-        "self": str(data.url),
-        "onboarding_url": onboarding_url,
-    }
-
-    meta = {"status_code": 201}
-
-    return ConnectedAccountResponse(links=links, meta=meta, data=data_response)
+        return HTTPException(status_code=401, detail="Unauthorized")
 
 
 @router.post("/connected-accounts/onboarding", status_code=201)
 async def connected_account_onboarding(
+    background_tasks: BackgroundTasks,
     data: Request,
     session: AsyncSession = Depends(get_db),
-    validated_developer: Dict[str, Any] = Depends(validate_developer),
 ):
     """continue onboarding"""
-    if data.headers.get("X-Developer-ID"):
-        client = redis_instance()
-    data_response = await continue_onboarding(
-        data=data, session=session, validated_developer=validated_developer
-    )
-    links = {
-        "self": str(data.url),
-    }
+    if data.headers.get("X-Developer-Token"):
+        developer_id = redis_instance().get(data.headers.get("X-Developer-Token"))
+        if developer_id:
+            data_response = await continue_onboarding(data=data, session=session)
+            links = {
+                "self": str(data.url),
+            }
 
-    meta = {"status_code": 201}
-    onboarding_url = data_response["onboarding_url"]
-    del data_response["onboarding_url"]
-    links["onboarding_url"] = onboarding_url
-    return ConnectedAccountResponse(links=links, meta=meta, data=data_response)
+            meta = {"status_code": 201}
+            onboarding_url = data_response["onboarding_url"]
+            del data_response["onboarding_url"]
+            links["onboarding_url"] = onboarding_url
+            return ConnectedAccountResponse(links=links, meta=meta, data=data_response)
+        else:
+            validated_developer = await validate_developer(
+                data, background_tasks=background_tasks
+            )
+            data_response = await continue_onboarding(
+                data=data, session=session, validated_developer=validated_developer
+            )
+            links = {
+                "self": str(data.url),
+            }
+            meta = {"status_code": 201}
+            onboarding_url = data_response["onboarding_url"]
+            del data_response["onboarding_url"]
+            links["onboarding_url"] = onboarding_url
+            return ConnectedAccountResponse(links=links, meta=meta, data=data_response)
+
+    else:
+        return HTTPException(status_code=401, detail="Unauthorized")
 
 
 @router.post("/connected-accounts/login", status_code=201)
