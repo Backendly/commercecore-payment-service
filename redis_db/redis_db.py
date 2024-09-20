e8ce9a8193695b88bf51a1bae2ccec35b595205e
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-20 23:19:37 +0100

    Refactor Redis configuration in redis_db.py

--- redis_db/redis_db.py
+++ redis_db/redis_db.py
@@ -1,29 +1,25 @@
-import aioredis
 import os
+import redis
 
 
 class CustomRedis:
     """Class Implementing Redis configuration"""
 
-    def __init__(self, client_url):
-        self.client_url = client_url
-        self.redis_client = None
+    def __init__(self):
+        self.client_url = redis.Redis()
 
-    async def initialize(self):
-        self.redis_client = await aioredis.from_url(self.client_url)
+    def set(self, key: str, value, expire: int = 3600):
+        self.client_url.set(key, value, ex=expire)
 
-    async def set(self, key: str, value, expire: int = 3600):
-        await self.redis_client.set(key, value, ex=expire)
+    def get(self, key: str):
+        return self.client_url.get(key)
 
-    async def get(self, key: str):
-        return await self.redis_client.get(key)
+    def delete(self, key: str):
+        self.client_url.delete(key)
 
-    async def delete(self, key: str):
-        await self.redis_client.delete(key)
-
-    async def exists(self, *keys):
+    def exists(self, *keys):
         existing_keys = []
         for key in keys:
-            if await self.redis_client.exists(key):
+            if self.client_url.exists(key):
                 existing_keys.append(key)
         return existing_keys
