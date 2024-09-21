1774e10c4234fd4736ce0879601d4e598154be0e
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-21 12:50:13 +0100

    Add webhooks_route.py to API v1

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -2,3 +2,4 @@ from .routes.account_routes import router as account_router
 from .routes.payment_method_routes import router as payment_method_router
 from .routes.refunds_routes import router as refunds_router
 from .routes.transaction_routes import router as transaction_router
+from .routes.webhooks_route import router as webhooks_router
