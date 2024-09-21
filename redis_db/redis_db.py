f5ce60759560344335f4bd29bc1699412ea5b55d
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-21 12:47:17 +0100

    Refactor Redis configuration to use environment variable for Redis URL

--- redis_db/redis_db.py
+++ redis_db/redis_db.py
@@ -6,7 +6,8 @@ class CustomRedis:
     """Class Implementing Redis configuration"""
 
     def __init__(self):
-        self.client_url = redis.Redis()
+        REDIS_URL = os.getenv("REDIS_URL")
+        self.client_url = redis.Redis.from_url(REDIS_URL)
 
     def set(self, key: str, value, expire: int = 3600):
         self.client_url.set(key, value, ex=expire)
@@ -23,3 +24,6 @@ class CustomRedis:
             if self.client_url.exists(key):
                 existing_keys.append(key)
         return existing_keys
+
+    def publish(self, channel: str, message: str):
+        self.client_url.publish(channel, message)
