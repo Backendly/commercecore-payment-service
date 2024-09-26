6ff088bb675f10f1c8a2cc046c15a1b4d6936e4f
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-09-26 08:53:30 +0100

    Refactor: Update authentication validation endpoints and headers in utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from fastapi.requests import Request
 import httpx
 from fastapi import HTTPException, status, BackgroundTasks
+import os
 
 
 from backgrounds import store_user_data, store_developer_data
@@ -8,51 +9,58 @@ from backgrounds import store_user_data, store_developer_data
 
 async def validate_developer(request: Request, background_tasks: BackgroundTasks):
     async with httpx.AsyncClient() as client:
-        client.base_url = "https://mock-auth.onrender.com/api/v1/"
+        client.base_url = os.getenv("AUTH_BASE_URL")
         developer_token = request.headers.get("X-Developer-Token")
-        app_id = request.headers.get("X-APP-ID")
-        headers_developer = {}
-        if developer_token:
-            headers_developer["Authorization"] = f"Bearer {developer_token}"
-        else:
-            raise HTTPException(401, detail="Unauthorized")
-
-        if app_id:
-            headers_developer["X-App-ID"] = app_id
-
-        response = await client.get("confirm/developer/", headers=headers_developer)
+        response = await client.get(
+            "developer/validate-token", headers={"x-api-token": developer_token}
+        )
         if response.status_code != 200:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized"
             )
         data = response.json()
-        key = f"dev_{developer_token}{app_id}" if app_id else f"dev_{developer_token}"
-        background_tasks.add_task(store_developer_data, key, data.get("developer_id"))
-        return data
+        key = f"dev_{developer_token}"
+        background_tasks.add_task(
+            store_developer_data, key, data.get("developer").get("id")
+        )
+        return {"developer_id": data.get("developer").get("id")}
+
+
+async def validate_app(request: Request, background_tasks: BackgroundTasks):
+    async with httpx.AsyncClient() as client:
+        client.base_url = os.getenv("AUTH_BASE_URL")
+        app_id = request.headers.get("x-APP-ID")
+        x_api_token = request.headers.get("X-Develoer-Token")
+        response = await client.get(
+            f"app/validate-app/{app_id}", headers={"x-api-token": x_api_token}
+        )
+        if response.status_code != 200:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized"
+            )
+        key = f"app_{app_id}{x_api_token}"
+        background_tasks.add_task(store_developer_data, key, app_id)
+        return {"app_id": app_id, "developer_token": x_api_token}
 
 
 async def validate_user(request: Request, background_tasks: BackgroundTasks):
     async with httpx.AsyncClient() as client:
-        client.base_url = "https://mock-auth.onrender.com/api/v1/"
+        client.base_url = os.getenv("AUTH_BASE_URL")
         user_id = request.headers.get("X-User-ID")
         app_id = request.headers.get("X-App-ID")
-        headers_user = {}
-        if user_id:
-            headers_user["X-User-ID"] = user_id
-        else:
-            raise HTTPException(
-                401,
-                detail="Please provide a valid user id",
-            )
-        if app_id:
-            headers_user["X-App-ID"] = request.headers.get("X-App-ID")
-        response = await client.get("confirm/user/", headers=headers_user)
+        x_api_token = request.headers.get("X-Token-ID")
+        headers_user = {
+            "x-api-token": x_api_token,
+            "x-app-id": app_id,
+        }
+        response = await client.get(
+            f"user/validate-user/{user_id}", headers=headers_user
+        )
         if response.status_code != 200:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail=f"Unauthorized",
             )
-        data = response.json()
-        key = f"usr_{user_id}{app_id}" if app_id else f"usr_{user_id}"
-        background_tasks.add_task(store_user_data, key, data.get("user_id"))
-        return data
+        key = f"user_{user_id}{app_id}"
+        background_tasks.add_task(store_user_data, key, user_id)
+        return {"user_id": user_id, "app_id": app_id}
