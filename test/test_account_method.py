ec616d887744d9fe342df0f3c44dff90a92df53b
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-10-01 00:26:57 +0100

    Add tests for creating connected accounts and onboarding users

--- test/test_account_method.py
+++ test/test_account_method.py
@@ -18,7 +18,22 @@ async def test_create_connected_account(client):
 async def test_account_onboarding_link(client):
     """This will test the onboarding of a user"""
     response = await client.post(
-        "/connected-accounts/onboarding", json={"account_id": "acct_1Q2aJc2azLnUiqXE"}
+        "/connected-accounts/onboarding",
+        json={"account_id": "acct_1Q2aJc2azLnUiqXE"},
+        headers={"X-Developer-Token": "b9f10f7c-4743-4423-a2c0-888168864f5d"},
     )
     data = response.json()
     assert response.status_code == 201
+    assert data != None
+
+
+@pytest.mark.anyio
+async def test_account_login_link_for_incomplete_onboarding(client):
+    """This will test account login success"""
+    response = await client.post(
+        "/connected-accounts/login",
+        json={"account_id": "acct_1Q2aJc2azLnUiqXE"},
+        headers={"X-Developer-Token": "b9f10f7c-4743-4423-a2c0-888168864f5d"},
+    )
+    data = response.json()
+    assert response.status_code == 500
