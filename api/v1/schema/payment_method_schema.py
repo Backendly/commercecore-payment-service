bf8c1f738b9de27afb011a7ad32484aa476e3b9d
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-10-21 07:57:27 +0100

    Refactor payment_method_schema.py to update PaymentMethodBase fields and remove unused validation methods

--- api/v1/schema/payment_method_schema.py
+++ api/v1/schema/payment_method_schema.py
@@ -6,77 +6,13 @@ import re
 
 
 class PaymentMethodBase(BaseModel):
-    type: PaymentMethodType
-    details: Dict[str, Any]
+    payment_method_id: str
+    type: str
+    preferred: bool
 
 
 class PaymentMethodCreate(PaymentMethodBase):
-    @model_validator(mode="before")
-    @classmethod
-    def check_for_required_details(cls, values):
-        details = values.get("details")
-        if not details:
-            raise ValueError("details is required")
-        type = values.get("type")
-        if not type:
-            raise ValueError("method_type is required")
-        if type == "card":
-            required_fields = [
-                "card_number",
-                "exp_month",
-                "exp_year",
-                "card_cvc",
-            ]
-            for field in required_fields:
-                if field not in details.keys():
-                    raise ValueError(f"{field} is required for card payment")
-        return values
-
-    @model_validator(mode="before")
-    @classmethod
-    def validate_expiry_date(cls, values):
-        details = values.get("details")
-        if not details:
-            raise ValueError("details is required")
-        type = values.get("type")
-        if type == "card" and details.get("expiry_date") is not None:
-            expiry_date = details.get("expiry_date")
-            if not re.match(r"^(0[1-9]|1[0-2])\/\d{2}$", expiry_date):
-                raise ValueError("expiry_date must be in MM/YY format")
-        return values
-
-    @model_validator(mode="before")
-    @classmethod
-    def validate_card_number(cls, values):
-        details = values.get("details")
-        if not details:
-            raise ValueError("details is required")
-        type = values.get("type")
-        if not details.get("card_number"):
-            raise ValueError("card_number is required for card payment")
-
-        if type == "card" and details.get("card_number") is not None:
-            card_number = details.get("card_number")
-            if card_number and not card_number.isdigit():
-                raise ValueError("card_number must be all digits")
-        return values
-
-    @model_validator(mode="before")
-    @classmethod
-    def validate_card_number_length(cls, values):
-        details = values.get("details")
-        if not details:
-            raise ValueError("details is required")
-        if not details.get("card_number"):
-            raise ValueError("card_number required for card payment")
-        type = values.get("type")
-        if type == "card" and details.get("card_number") is not None:
-            card_number = details.get("card_number")
-            if card_number and (
-                len(str(card_number)) < 13 or len(str(card_number)) > 19
-            ):
-                raise ValueError("card_number must be between 13 and 19 digits")
-        return values
+    pass
 
 
 class PaymentMethodInDB(PaymentMethodBase):
