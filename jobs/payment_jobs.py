d131127a06d4e641052151a19334ecf700a43efc
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-10-28 14:10:26 +0100

    Refactor payment_jobs.py to update payment processing logic and add payment confirmation task

--- jobs/payment_jobs.py
+++ jobs/payment_jobs.py
@@ -7,20 +7,25 @@ from celery_config import celery_app
 from celery_config import LocalSession
 from models.payment_method_model import PaymentMethod
 from models.payment_method_model import ConnectedAccount
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 
-@celery_app.task(bind=True)
-def receive_orders(self):
-    """Receives orders from the payment_status channel (synchronously)"""
-    client = redis_instance()
-    pubsub = client.subscribe("payment_order_created")
-    try:
-        for message in pubsub.listen():
-            if message["type"] == "message":
-                data = json.loads(message["data"])
-                process_order.delay(data)
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"{e}")
+# @celery_app.task(bind=True)
+# def recieve_orders(self):
+#     """Standalone Redis Pub/Sub listener that triggers Celery tasks."""
+#     client = redis_instance()
+#     pubsub = client.subscribe("payment_order_created")
+#     logging.info("Listening to payment_order_created channel...")
+#     try:
+#         for message in pubsub.listen():
+#             if message["type"] == "message":
+#                 data = json.loads(message["data"])
+#                 process_order.delay(data)  # Submit task to Celery
+#     except Exception as e:
+#         logging.error(f"Error receiving orders: {e}")
+#         raise HTTPException(status_code=400, detail=str(e))
 
 
 @celery_app.task(bind=True)
@@ -34,12 +39,11 @@ def process_order(self, data):
     amount = round(float(amount) * 100)
     try:
         session = LocalSession()
-        account_id = (
-            session.query(ConnectedAccount)
-            .filter_by(developer_id=developer_id)
-            .first()
-            .account_id
+        connected_account = (
+            session.query(ConnectedAccount).filter_by(developer_id=developer_id).first()
         )
+
+        account_id = connected_account.account_id
         payment_intent = stripe.PaymentIntent.create(
             stripe_account=account_id,
             amount=amount,
@@ -50,18 +54,22 @@ def process_order(self, data):
                 "app_id": app_id,
                 "user_id": user_id,
             },
+            automatic_payment_methods={"enabled": True, "allow_redirects": "never"},
         )
-        confirm_payment.delay(payment_intent)
+
+        logging.info("Proceeding to Get Confirmation of Payment...")
+        confirm_payment.delay(payment_intent, account_id)
     except stripe.error.StripeError as e:
         logging.error(f"Stripe error: {str(e)}")
-        self.retry(exc=e, countdown=60)
+        self.retry(exc=e, countdown=60, max_retries=5)
     finally:
         session.close()
 
 
-@celery_app.task(bind=True)
-def confirm_payment(self, data):
+@celery_app.task(bind=True, default_retry_delay=60, max_retries=5)
+def confirm_payment(self, data, account_id):
     """Confirms the payment"""
+    logging.info("Starting Payment Confirmation...")
     try:
         developer_id = data.get("metadata").get("developer_id")
         user_id = data.get("metadata").get("user_id")
@@ -79,11 +87,11 @@ def confirm_payment(self, data):
             .payment_method_id
         )
         payment_intent = stripe.PaymentIntent.confirm(
-            data.get("id"), payment_method=payment_method_id
+            data.get("id"), payment_method=payment_method_id, stripe_account=account_id
         )
     except stripe.error.StripeError as e:
         logging.error(f"Stripe error: {str(e)}")
-        self.retry(exc=e, countdown=60)
+        self.retry(exc=e, countdown=1200)
 
     finally:
         session.close()
